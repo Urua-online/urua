87170dcffb80fce9dd3170793b942c736b668fbe
Tingwutobong Okonson <tidarkson@gmail.com>
2023-10-03 09:24:36 +0100

    ...

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,7 +8,12 @@ import { FaBars, FaTimes } from 'react-icons/fa'
 export const Navbar = () => {
 
   const navRef = useRef()
+
   const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav")
+  }
+
+  const hideNavbar = () => {
     navRef.current.classList.toggle("responsive_nav")
   }
 
@@ -20,16 +25,16 @@ export const Navbar = () => {
           <Link to='/'><img src={logo2} alt="" className='w-16 pt-1'/></Link>
         </div>
 
-        <nav className='flex flex-col md:flex-row items-center justify-center gap-8' ref={navRef}>
+        <nav className='flex flex-col md:flex-row items-center justify-center gap-8 second-background' ref={navRef}>
           <div className='flex items-center justify-center gap-2 absolute top-8 left-6 md:hidden'>
             <img src={logo1} alt="" className='w-10'/>
             <Link to='/'><img src={logo2} alt="" className='w-16 pt-1'/></Link>
           </div>
           
           <ul className='flex flex-col md:flex-row items-center justify-between gap-4 md:gap-8'>
-            <li className='hover:text-red-500'><HashLink to='/'>Home</HashLink></li>
-            <li className='hover:text-red-500'><HashLink to='#about'>About</HashLink></li>
-            <li className='hover:text-red-500'><HashLink to='#FAQ'>FAQs</HashLink></li>
+            <li className='hover:text-red-500' onClick={hideNavbar}><HashLink to='/' smooth>Home</HashLink></li>
+            <li className='hover:text-red-500' onClick={hideNavbar}><HashLink to='/#about' smooth>About</HashLink></li>
+            <li className='hover:text-red-500' onClick={hideNavbar}><HashLink to='/#FAQ' smooth>FAQs</HashLink></li>
           </ul>
           <button className='urua text-white px-6 py-1 rounded-br-md'><a href="https://docs.google.com/forms/d/e/1FAIpQLSfqKhKRKz2ooNXORdZ7kaQt1LNtSH749_N29YoAUzKNQq_RJA/viewform?usp=sf_link">Shop Now</a></button>
 
